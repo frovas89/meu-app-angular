e4c85122405a8723e78379b0e585cf0d28320777
frovas89 <frovas@gmail.com>
2025-07-29 19:34:48 -0300

    começando

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Api } from './services/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('meu-app-angular');
+export class App implements OnInit{
+   title = 'Consumindo API com Angular (Standalone)';
+  public objects: any[] = [];
+
+  // A lógica do construtor e do ngOnInit é a mesma
+  constructor(private apiService: Api) {}
+
+  ngOnInit(): void {
+    this.carregarObjetos();
+  }
+
+  public carregarObjetos(): void {
+    this.apiService.getObjects().subscribe(
+      (data: any[]) => {
+        this.objects = data;
+        console.log(this.objects);
+      },
+      (error) => {
+        console.error('Erro ao buscar objetos:', error);
+      }
+    );
+  }
 }
